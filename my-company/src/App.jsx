07dfc59e6af039cde58ecc1e67f4ee70bec78a7f
emanuelnakeng <emanuelnakeng@gmail.com>
2024-08-18 18:31:19 +0200

    Fixed navbar name

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/NavBar';
+import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Services from './components/Services';
