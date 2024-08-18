8a3515efe1dee598e566a4829eeb3864d8df2bf8
emanuelnakeng <emanuelnakeng@gmail.com>
2024-08-18 18:28:49 +0200

    Added router

---
+++ my-company/src/App.jsx
@@ -0,0 +1,48 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/NavBar';
+import Home from './components/Home';
+import About from './components/About';
+import Services from './components/Services';
+import Contact from './components/Contact';
+import NoPage from './components/NoPage';
+
+function App() {
+	console.log('wahat the fuck');
+
+	return (
+		<BrowserRouter>
+			<Navbar />
+			<Routes>
+				<Route>
+					<Route index element={<Home />} />
+					<Route path='/about' element={<About />} />
+					<Route path='/services' element={<Services />} />
+					<Route path='/about' element={<About />} />
+					<Route path='/contact' element={<Contact />} />
+					<Route path='*' element={<NoPage />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
+}
+
+export default App;
+
+// <Router>
+{
+	/* <Switch>
+				<Route exact path='/'>
+					<Home />
+				</Route>
+				<Route exact path='/about'>
+					<About />
+				</Route>
+				<Route exact path='/services'>
+					<Services />
+				</Route>
+				<Route exact path='/about'>
+					<About />
+				</Route>
+			</Switch> */
+}
+// </Router>
---
+++ my-company/src/components/NoPage.jsx
@@ -0,0 +1,9 @@
+function NoPage() {
+	return (
+		<div>
+			<h3>Page not found</h3>
+		</div>
+	);
+}
+
+export default NoPage;
