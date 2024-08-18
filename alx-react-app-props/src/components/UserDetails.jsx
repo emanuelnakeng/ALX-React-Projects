b9c8e952ba407fb3194c7030e0c39a36eae16ae0
emanuelnakeng <emanuelnakeng@gmail.com>
2024-08-18 17:45:33 +0200

    Added context

---
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import UserContext from './UserContext';
+
+function UserDetails({UserContext}) {
+    return (
+      <div>
+        <p>Name: {UserContext.name}</p>
+        <p>Email: {UserContext.email}</p>
+      </div>
+    );
+}
+  
+export default UserDetails;
