a06c798e9155d7c30f4e4a519b0677e985dcf5f2
emanuelnakeng <emanuelnakeng@gmail.com>
2024-09-07 14:19:42 +0200

    Fixed profile

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,19 +1,41 @@
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Switch, Route, Link, useRouteMatch } from 'react-router-dom';
+import {
+	BrowserRouter as Routes,
+	Switch,
+	Route,
+	Link,
+	Redirect,
+} from 'react-router-dom';
+import ProfileDetails from './ProfileDetails';
+import ProfileSettings from './ProfileSettings';
 
 function Profile() {
+	let { path, url } = useRouteMatch();
+
 	return (
 		<div>
-			<h1>Profile</h1>
-			<nav>
-				<ul>
-					<li>
-						<Link to='details'>Details</Link>
-					</li>
-					<li>
-						<Link to='settings'>Settings</Link>
-					</li>
-				</ul>
-			</nav>
+			<h2>Profile</h2>
+			<ul>
+				<li>
+					<Link to={`${url}/details`}>Profile Details</Link>
+				</li>
+				<li>
+					<Link to={`${url}/settings`}>Profile Settings</Link>
+				</li>
+			</ul>
+
+			<Switch>
+				<Route exact path={path}>
+					<h3>Please select a sub-section.</h3>
+				</Route>
+				<Route path={`${path}/details`}>
+					<ProfileDetails />
+				</Route>
+				<Route path={`${path}/settings`}>
+					<ProfileSettings />
+				</Route>
+			</Switch>
 		</div>
 	);
 }
